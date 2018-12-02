bc540e8f0e4ac9b2aacc9781cb64ef3b29480f8d
Mengying Long <531107066@qq.com>
2018-12-02 23:19:29 +0800

    the side bar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+ <el-container class="conbody">
+   <el-header>
+      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+         <el-radio-button :label="false"><i class="el-icon-arrow-left"></i></el-radio-button>
+         <el-radio-button :label="true"><i class="el-icon-arrow-right"></i></el-radio-button>
+      </el-radio-group>
+   </el-header>
+   <el-container class="conbody">
+     <el-aside width="200px">
+
+     <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+       <el-menu-item index="1">
+                <i class="el-icon-search"></i>
+                <span slot="title">Search</span>
+              </el-menu-item>
+       <el-menu-item index="2">
+         <i class="el-icon-document"></i>
+         <span slot="title">Report</span>
+       </el-menu-item>
+       <el-menu-item index="3">
+         <i class="el-icon-document"></i>
+         <span slot="title">导航三</span>
+       </el-menu-item>
+       <el-menu-item index="4">
+         <i class="el-icon-setting"></i>
+         <span slot="title">导航四</span>
+       </el-menu-item>
+     </el-menu>
+     </el-aside>
+     <el-main>Main</el-main>
+   </el-container>
+ </el-container>
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+   data() {
+        return {
+          isCollapse: true
+        };
+      },
+   methods: {
+        handleOpen(key, keyPath) {
+          console.log(key, keyPath);
+        },
+        handleClose(key, keyPath) {
+          console.log(key, keyPath);
+        }
+      }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+
+.conbody{
+height:100%;
+overflow: hidden;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+.el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+    overflow:hidden;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-Vue.use(Element, { size: 'small', zIndex: 3000 });
+//Vue.use(Element, { size: 'small', zIndex: 3000 });
 
 /* eslint-disable no-new */
 new Vue({
@@ -17,3 +17,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
